c43620346d57186ca40f08220355e1f42c18d9cd
fenn7 <afenn666@gmail.com>
2022-11-16 16:00:33 +0000

    basic functionality

--- lessons/forms.py
+++ lessons/forms.py
@@ -8,3 +8,7 @@ class SignUpForm(forms.ModelForm):
 
     password = forms.CharField(label='Password', widget=forms.PasswordInput())
     password_confirm = forms.CharField(label='Re-enter password', widget=forms.PasswordInput())
+    
+class LogInForm(forms.Form):
+    email = forms.CharField(label = "Email")
+    password = forms.CharField(label='Password', widget=forms.PasswordInput())
--- lessons/views.py
+++ lessons/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .forms import SignUpForm
+from .forms import LogInForm
 # Create your views here.
 
 def home(request):
@@ -8,3 +9,7 @@ def home(request):
 def sign_up(request):
     form = SignUpForm()
     return render(request, 'sign_up.html', {'form': form})
+
+def log_in(request):
+    form = LogInForm()
+    return render(request, 'log_in.html', {'form': form})
