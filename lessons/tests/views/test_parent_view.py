fc4a5bd6daf68afdc11c151195c2bf9148f719be
fenn7 <afenn666@gmail.com>
2022-12-08 12:45:33 +0000

    add separate parent sign up page

--- lessons/tests/views/test_parent_view.py
+++ lessons/tests/views/test_parent_view.py
@@ -5,27 +5,64 @@ from lessons.tests.helpers import LogInTester
 
 # Create your tests here.
 class LogInTest(TestCase, LogInTester):
-    def setUp(self):
-        self.sample_email = "parent@parents.com"
-        self.user = User.objects.create_user(
-            email=self.sample_email,
-            first_name="A",
-            last_name="Parent",
-            password="password",
-        )
-        self.user.is_parent = True
-        self.user.save()
     
+    fixtures = [ 
+                "lessons/tests/fixtures/default_parent.json",
+                "lessons/tests/fixtures/other_users.json"  
+                ]
+
+    def setUp(self):
+        super(TestCase, self).setUp()
+        self.parent = User.objects.get(email="par@ent.org")
+        self.child = User.objects.get(email="other.user@example.org")
+        self.child_form_details = {
+            "email": self.child.email,
+            "password": "Watermelon123"
+        }
+        self.reg_page = reverse('register_child')
+        self.sel_page = reverse('select_child')
+    
+    def test_urls_correct(self):
+        self.assertEqual(self.reg_page, '/account/register_child/')
+        self.assertEqual(self.sel_page, '/account/select_child/')
+        
     def test_parent_form_login(self):
-        valid_pass = (self.sample_email, "password")
         self.assertTrue(
-            self.client.login(username=valid_pass[0], password=valid_pass[1])
+            self.client.login(email=self.parent.email, password="Watermelon123")
         )
         self.assertTrue(self.is_logged_in())
-        self.assertTrue(self.user.is_parent)
     
-    def test_parent_register_child(self):
-        register_child_page = reverse('register_child')
-        self.assertEqual(register_child_page, '/account/register_child/')
-        # response = self.client.get(register_child_page, follow=True, data=self.user.is_parent)
+    def test_non_parent_cannot_register_child(self):
+        self.client.login(email=self.child.email, password="Watermelon123")
+        response = self.client.get(self.reg_page)
+        self.assertEqual(response.status_code, 302)
+        
+    def test_parent_can_access_register_child(self):
+        self.client.login(email=self.parent.email, password="Watermelon123")
+        response = self.client.get(self.reg_page)
+        self.assertEqual(response.status_code, 200)
+     
+    def test_parent_can_register_child(self):
+        self.client.login(email=self.parent.email, password="Watermelon123")
+        response = self.client.post(self.reg_page, self.child_form_details, follow=True)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.parent.children.all().first(), self.child)
+        self.assertEqual(self.child.parents.all().first(), self.parent)
+    
+    def test_non_parent_cannot_select_child(self):
+        self.client.login(email=self.child.email, password="Watermelon123")
+        response = self.client.get(self.sel_page)
+        self.assertEqual(response.status_code, 302)
+        
+    def test_parent_can_access_select_child(self):
+        self.client.login(email=self.parent.email, password="Watermelon123")
+        response = self.client.get(self.sel_page)
+        self.assertEqual(response.status_code, 200)
+     
+    def test_parent_can_select_child(self):
+        self.client.login(email=self.parent.email, password="Watermelon123")
+        self.client.post(self.reg_page, self.child_form_details, follow=True)
+        self.client.get(reverse('account'))
+        response = self.client.post(self.sel_page, {"email": self.child.email}, follow=True)
+        self.assertEqual(response.status_code, 200)
         
